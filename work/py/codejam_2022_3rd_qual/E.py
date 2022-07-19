32e377c8214f4768f6ba260399db8350e43f454e
loegnah <loegnah@gmail.com>
2022-07-20 01:49:33 +0900

    Fail E

--- work/py/codejam_2022_3rd_qual/E.py
+++ work/py/codejam_2022_3rd_qual/E.py
@@ -1,26 +1,37 @@
 import sys
 
 readline = sys.stdin.readline
-MX = pow(10, 9) + 7
+MX = float('inf')
+subSums = [[0] * 2005 for _ in range(2005)]
+subSums2 = [[0] * 2005 for _ in range(2005)]
 
 
 def calc(i, j):
     vi, vj = nums[i], nums[j]
-    ret = (A * (vi ^ vj) + B * (vi & vj) + C * (vi | vj)) % MOD
+    return (A * (vi ^ vj) + B * (vi & vj) + C * (vi | vj)) % MOD
+
+
+def calc_range(a, b):
+    ret = 0
+    for i in range(a, b):
+        ret = (ret + subSums[i][b])
     return ret
-    # return (A * (vi ^ vj) + B * (vi & vj) + C * (vi | vj)) % MOD
 
 
 def dp(st, en):
+    if cache[st][en] != -1:
+        return cache[st][en]
+    ret = 0
     if st == en:
-        return 0
-    # if st == en - 1:
-    #     return calc(st, en)
-
-    ret = MX
-    for k in range(st, en):
-        ret = min(ret, calc(k, k + 1) + dp(st, k) + dp(k + 1, en))
-    print(ret)
+        ret = 0
+    elif st == en - 1:
+        ret = subSums2[st][en]
+    else:
+        mn = float('inf')
+        for k in range(st, en):
+            mn = min(mn, dp(st, k) + dp(k + 1, en))
+        ret = mn + subSums2[st][en]
+    cache[st][en] = ret
     return ret
 
 
@@ -28,5 +39,19 @@ for _ in range(int(readline())):
     n = int(readline())
     A, B, C, MOD = map(int, readline().split())
     nums = list(map(int, readline().split()))
+    cache = [[-1] * n for _ in range(n + 1)]
+
+    if n == 1:
+        print(0)
+        continue
+
+    for p in range(n):
+        subSums[p][p] = 0
+        for q in range(p + 1, n):
+            subSums[p][q] = subSums[p][q - 1] + calc(p, q)
+
+    for p in range(n):
+        for q in range(n):
+            subSums2[p][q] = calc_range(p, q)
 
     print(dp(0, n - 1))
