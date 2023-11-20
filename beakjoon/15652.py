918f3f6544c572d666cb744ebaf3c4d74f64c0a2
loegnah <loegnah@gmail.com>
2023-11-20 16:49:59 +0900

    [beakjoon] 15651, 15652

---
+++ beakjoon/15652.py
@@ -0,0 +1,18 @@
+import sys
+from itertools import combinations_with_replacement
+
+readline = sys.stdin.readline
+
+
+def solve(N: int, M: int):
+    nums = [k for k in range(1, N + 1)]
+    for comb in combinations_with_replacement(nums, M):
+        print(" ".join(map(str, comb)))
+
+
+def main():
+    N, M = map(int, readline().split())
+    solve(N, M)
+
+
+main()
