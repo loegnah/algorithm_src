604bf5a80bd4ea56203fd7d3b981e73299bda44a
loegnah <loegnah@gmail.com>
2023-12-03 23:44:11 +0900

    Resolve(baekjoon): 2630

--- beakjoon/2630.py
+++ beakjoon/2630.py
@@ -1,29 +1,44 @@
 import sys
+from collections import deque
+
+readline = sys.stdin.readline
 
 
-def isAllSame(sY, sX, length):
-    color = board[sY][sX]
-    for y in range(sY, sY + length):
-        for x in range(sX, sX + length):
-            if board[y][x] != color:
-                nextLn = length // 2
-                isAllSame(sY, sX, nextLn)
-                isAllSame(sY + nextLn, sX, nextLn)
-                isAllSame(sY, sX + nextLn, nextLn)
-                isAllSame(sY + nextLn, sX + nextLn, nextLn)
-                return
-    ans[int(color)] += 1
+def solve(N: int, papers: list[list[int]]):
+    qu = deque()
+    qu.append((0, 0, N))
+
+    def isAllSame():
+        sy, sx, sz = qu.popleft()
+        baseColor = papers[sy][sx]
+        for y in range(sy, sy + sz):
+            for x in range(sx, sx + sz):
+                if papers[y][x] != baseColor:
+                    szHalf = sz // 2
+                    qu.append((sy, sx, szHalf))
+                    qu.append((sy, sx + szHalf, szHalf))
+                    qu.append((sy + szHalf, sx, szHalf))
+                    qu.append((sy + szHalf, sx + szHalf, szHalf))
+                    return -1
+        return baseColor
+
+    white, blue = 0, 0
+    while qu:
+        checkRet = isAllSame()
+        if checkRet == -1:
+            continue
+        if checkRet == 0:
+            white += 1
+        else:
+            blue += 1
+    print(white)
+    print(blue)
 
 
-if __name__ == '__main__':
-    readline = sys.stdin.readline
+def main():
     N = int(readline())
-    board = []
-    ans = [0, 0]
+    papers = [list(map(int, readline().split())) for _ in range(N)]
+    solve(N, papers)
 
-    for _ in range(N):
-        board.append(list(map(int, readline().split())))
 
-    isAllSame(0, 0, N)
-    print(ans[0])
-    print(ans[1])
+main()
