30f992ac6a143d4dca63abb3e3fc4a117a186a18
hangeol.park <hangeol.park@lge.com>
2026-04-28 19:16:30 +0900

    refactor: simplify c solution and improve g code organization
    - c.py: Remove complex rerooting DP, use simpler tree DP approach
    - c_v2.py: Delete (simpler version is now the main solution)
    - g.py: Extract tree construction and DP into separate functions, change MOD to 998244353

--- codejam_lge/2026-final-r1/c.py
+++ codejam_lge/2026-final-r1/c.py
@@ -1,6 +1,8 @@
 import sys
 from collections import deque
 
+readline = sys.stdin.readline
+
 def bfs(adj, root, N):
     parent = [-1] * N
     order = []
@@ -18,105 +20,32 @@ def bfs(adj, root, N):
     return parent, order
 
 
-def subtree_dp(adj, parent, order, N):
-    dp_up = [1] * N
-    dp_dn = [1] * N
-    top_up = [[] for _ in range(N)]
-    top_dn = [[] for _ in range(N)]
-
-    for v in reversed(order):
-        up_vals = []
-        dn_vals = []
-        for u in adj[v]:
-            if parent[u] != v:
-                continue
-            if u > v:
-                val = 1 + dp_dn[u]
-                if val > dp_up[v]:
-                    dp_up[v] = val
-                up_vals.append((val, u))
-            else:
-                val = 1 + dp_up[u]
-                if val > dp_dn[v]:
-                    dp_dn[v] = val
-                dn_vals.append((val, u))
-        up_vals.sort(reverse=True)
-        top_up[v] = up_vals[:2]
-        dn_vals.sort(reverse=True)
-        top_dn[v] = dn_vals[:2]
-
-    return dp_up, dp_dn, top_up, top_dn
-
-
-def best_excluding(top, from_parent, c):
-    best = 1
-    if top:
-        if top[0][1] != c:
-            best = top[0][0]
-        elif len(top) >= 2:
-            best = top[1][0]
-    if from_parent > best:
-        best = from_parent
-    return best
-
-
-def reroot(adj, parent, order, dp_up, dp_dn, top_up, top_dn):
-    root = order[0]
-    from_parent_up = [0] * len(order)
-    from_parent_dn = [0] * len(order)
-
-    ans = max(dp_up[root], dp_dn[root])
-    if len(top_up[root]) >= 2:
-        ans = max(ans, top_up[root][0][0] + top_up[root][1][0] - 1)
-    if len(top_dn[root]) >= 2:
-        ans = max(ans, top_dn[root][0][0] + top_dn[root][1][0] - 1)
-
-    for v in order:
-        for c in adj[v]:
-            if parent[c] != v:
-                continue
-
-            best_up_excl = best_excluding(top_up[v], from_parent_up[v], c)
-            best_dn_excl = best_excluding(top_dn[v], from_parent_dn[v], c)
-
-            if v > c:
-                from_parent_up[c] = 1 + best_dn_excl
-            else:
-                from_parent_dn[c] = 1 + best_up_excl
-
-            cur = max(
-                max(dp_up[c], from_parent_up[c]),
-                max(dp_dn[c], from_parent_dn[c]),
-            )
-            if cur > ans:
-                ans = cur
-
-            for direction in ("up", "dn"):
-                top = top_up[c] if direction == "up" else top_dn[c]
-                fp = from_parent_up[c] if direction == "up" else from_parent_dn[c]
-                cands = [val for val, _ in top[:2]]
-                if fp > 0:
-                    cands.append(fp)
-                    cands.sort(reverse=True)
-                if len(cands) >= 2:
-                    combined = cands[0] + cands[1] - 1
-                    if combined > ans:
-                        ans = combined
-
-    return ans
-
-
 def solve(N, adj):
     if N <= 2:
         return N
 
     parent, order = bfs(adj, 0, N)
-    dp_up, dp_dn, top_up, top_dn = subtree_dp(adj, parent, order, N)
-    return reroot(adj, parent, order, dp_up, dp_dn, top_up, top_dn)
+
+    dp_up = [1] * N
+    dp_dn = [1] * N
+
+    for v in reversed(order):
+        for u in adj[v]:
+            if parent[u] != v:
+                continue
+            if u > v:
+                dp_up[v] = max(dp_up[v], 1 + dp_dn[u])
+            else:
+                dp_dn[v] = max(dp_dn[v], 1 + dp_up[u])
+
+    ans = 0
+    for v in range(N):
+        ans = max(ans, dp_up[v], dp_dn[v])
+
+    return ans
 
 
 if __name__ == "__main__":
-    readline = sys.stdin.readline
     N = int(readline())
     adj = [[] for _ in range(N)]
     for _ in range(N - 1):
--- codejam_lge/2026-final-r1/g.py
+++ codejam_lge/2026-final-r1/g.py
@@ -1,16 +1,20 @@
 import sys
-from operator import mul
 
-MOD = 987654321
+MOD = 998244353
+
+readline = sys.stdin.readline
+
+def vec_add(a, b):
+    return [(x + y) % MOD for x, y in zip(a, b)]
 
 
-def solve(N, M, V, adj):
+def make_tree(N, adj):
     children = [[] for _ in range(N + 1)]
     visited = [False] * (N + 1)
-    queue = [1]
     visited[1] = True
-    bfs_order = []
+    queue = [1]
     head = 0
+    bfs_order = []
     while head < len(queue):
         u = queue[head]
         head += 1
@@ -20,70 +24,75 @@ def solve(N, M, V, adj):
                 visited[nb] = True
                 children[u].append(nb)
                 queue.append(nb)
+    return children, bfs_order
 
+
+def dp(N, M, V, children, bfs_order):
+    zero = [0] * M
     size = [1] * (N + 1)
     P_total = [0] * (N + 1)
-    zero_vec = [0] * M
-    pair = [zero_vec] * (N + 1)
-    A = [zero_vec] * (N + 1)
-    B = [zero_vec] * (N + 1)
+    pair = [zero] * (N + 1)
+    A = [zero] * (N + 1)
+    B = [zero] * (N + 1)
 
     for u in reversed(bfs_order):
         ch = children[u]
-        lc = len(ch)
-        if lc == 1:
+        if len(ch) == 1:
             c = ch[0]
             size[u] = 1 + size[c]
             P_total[u] = P_total[c]
             pair[u] = pair[c]
             A[u] = A[c]
             B[u] = B[c]
-        elif lc == 2:
+        elif len(ch) == 2:
             d1, d2 = ch
-            s1 = size[d1]
-            s2 = size[d2]
+            s1, s2 = size[d1], size[d2]
+            vc = V[u] - 1
             size[u] = 1 + s1 + s2
-            v_color = V[u] - 1
-            p1 = pair[d1]
-            p2 = pair[d2]
-            new_pair = [(a + b) % MOD for a, b in zip(p1, p2)]
-            new_pair[v_color] = (new_pair[v_color] + s1 * s2) % MOD
-            pair[u] = new_pair
             P_total[u] = (P_total[d1] + P_total[d2] + s1 * s2) % MOD
-            tri_vc = (s1 * p2[v_color] + s2 * p1[v_color]) % MOD
+
+            p = vec_add(pair[d1], pair[d2])
+            p[vc] = (p[vc] + s1 * s2) % MOD
+            pair[u] = p
+
             tt = (s1 * P_total[d2] + s2 * P_total[d1]) % MOD
-            a1 = A[d1]
-            a2 = A[d2]
-            new_A = [(a + b) % MOD for a, b in zip(a1, a2)]
-            new_A[v_color] = (new_A[v_color] + tt) % MOD
-            A[u] = new_A
-            b1 = B[d1]
-            b2 = B[d2]
-            new_B = [(a + b) % MOD for a, b in zip(b1, b2)]
-            new_B[v_color] = (new_B[v_color] + tri_vc) % MOD
-            B[u] = new_B
+            a = vec_add(A[d1], A[d2])
+            a[vc] = (a[vc] + tt) % MOD
+            A[u] = a
+
+            tri = (s1 * pair[d2][vc] + s2 * pair[d1][vc]) % MOD
+            b = vec_add(B[d1], B[d2])
+            b[vc] = (b[vc] + tri) % MOD
+            B[u] = b
+
+    return size, P_total, pair, A, B
+
+
+def solve(N, M, V, adj):
+    children, bfs_order = make_tree(N, adj)
+    size, P_total, pair, A, B = dp(N, M, V, children, bfs_order)
 
     answer = 0
     for u in range(1, N + 1):
         if len(children[u]) != 2:
             continue
         c1, c2 = children[u]
-        c_L = V[u] - 1
-        p1 = pair[c1]
-        p2 = pair[c2]
+        vc = V[u] - 1
+
         caseA_1 = P_total[c1] * P_total[c2] % MOD
-        caseA_2 = sum(map(mul, p1, p2)) % MOD
+        caseA_2 = sum(map(lambda x, y: x * y, pair[c1], pair[c2])) % MOD
         caseA = (8 * caseA_1 + 16 * caseA_2) % MOD
-        caseB_1 = (size[c1] * A[c2][c_L] + size[c2] * A[c1][c_L]) % MOD
-        caseB_2 = (size[c1] * B[c2][c_L] + size[c2] * B[c1][c_L]) % MOD
+
+        caseB_1 = (size[c1] * A[c2][vc] + size[c2] * A[c1][vc]) % MOD
+        caseB_2 = (size[c1] * B[c2][vc] + size[c2] * B[c1][vc]) % MOD
         caseB = (8 * caseB_1 + 16 * caseB_2) % MOD
+
         answer = (answer + caseA + caseB) % MOD
 
     return answer
 
 
 if __name__ == "__main__":
-    readline = sys.stdin.readline
     N, M = map(int, readline().split())
     V = [0] + list(map(int, readline().split()))
     adj = [[] for _ in range(N + 1)]
